3a3dfcdcba56c9524cbbe656e474a0484f8ad07f
Vojislav Trujkic <voizlav@gmail.com>
2023-02-21 13:07:34 +0100

    Update greatest_number

--- part4/09_greatest_number/greatest_number.py
+++ part4/09_greatest_number/greatest_number.py
@@ -1,8 +1,6 @@
 def greatest_number(num1, num2, num3):
-    if num1 > num2:
-        if num1 > num3:
-            return num1
-        return num3
+    if num1 > num2 and num1 > num3:
+        return num1
     elif num2 > num3:
         return num2
     return num3
