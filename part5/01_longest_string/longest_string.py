5e8cf8fed5bd263cb09ff5ae138ed844f6a86e6e
Vojislav Trujkic <voizlav@gmail.com>
2023-02-22 22:32:10 +0100

    Add longest_string

---
+++ part5/01_longest_string/longest_string.py
@@ -0,0 +1,7 @@
+def longest(strings: list):
+    return max(strings, key=len)
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
