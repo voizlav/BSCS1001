f8352f19dffcd3b363cc0d8961d8fdb0c1632790
Vojislav Trujkic <voizlav@gmail.com>
2023-02-22 23:24:50 +0100

    Update go

--- part5/03_go/go.py
+++ part5/03_go/go.py
@@ -1,12 +1,12 @@
 def who_won(game_board: list):
-    player1, player2 = 0, 0
+    p1, p2 = 0, 0
 
     for row in game_board:
         for box in row:
-            player1 += 1 if box == 1 else 0
-            player2 += 1 if box == 2 else 0
+            p1 += 1 if box == 1 else 0
+            p2 += 1 if box == 2 else 0
     
-    return 1 if player1 > player2 else (2 if player2 > player1 else 0)
+    return 1 if p1 > p2 else (2 if p2 > p1 else 0)
 
 
 if __name__ == "__main__":
