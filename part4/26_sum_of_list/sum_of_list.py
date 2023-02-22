20bf19ea3ad203779d848bb507d9a14480da170c
Vojislav Trujkic <voizlav@gmail.com>
2023-02-22 01:39:16 +0100

    Update sum_of_list

--- part4/26_sum_of_list/sum_of_list.py
+++ part4/26_sum_of_list/sum_of_list.py
@@ -1,9 +1,6 @@
 def list_sum(items1: list, items2: list) -> list:
-    result = []
-    for x in range(len(items1)):
-        result.append(items1[x] + items2[x])
-    return result
+    return [items1[x] + items2[x] for x in range(len(items1))]
 
 
 if __name__ == "__main__":
-    print(list_sum([1,2,3], [1,2,3]))
+    print(list_sum([1, 2, 3], [1, 2, 3]))
