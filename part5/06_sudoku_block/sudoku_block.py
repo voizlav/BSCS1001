b19b5a0474842e3222c3dc8ae1207b6f9106e672
Vojislav Trujkic <voizlav@gmail.com>
2023-02-24 04:30:45 +0100

    Add sudoku_block

---
+++ part5/06_sudoku_block/sudoku_block.py
@@ -0,0 +1,27 @@
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    numbers = []
+
+    for row in sudoku[row_no:row_no + 3]:
+        for box in row[column_no:column_no + 3]:
+            if box > 0 and box in numbers:
+                return False
+            numbers.append(box)
+    return True
+        
+
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(block_correct(sudoku, 0, 0))
+    print(block_correct(sudoku, 1, 2))
