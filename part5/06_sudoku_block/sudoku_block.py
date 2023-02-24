90d026274add46b4401ed4c8908eb8204239005d
Vojislav Trujkic <voizlav@gmail.com>
2023-02-24 04:31:25 +0100

    Update sudoku_block

--- part5/06_sudoku_block/sudoku_block.py
+++ part5/06_sudoku_block/sudoku_block.py
@@ -1,4 +1,4 @@
-def block_correct(sudoku: list, row_no: int, column_no: int):
+def block_correct(sudoku: list, row_no: int, column_no: int) -> bool:
     numbers = []
 
     for row in sudoku[row_no:row_no + 3]:
