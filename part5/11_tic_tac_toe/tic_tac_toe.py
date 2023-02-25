a010cacee96af87663899e5313072aafcb2d5fb0
Vojislav Trujkic <voizlav@gmail.com>
2023-02-25 04:46:17 +0100

    Add tic_tac_toe

---
+++ part5/11_tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,12 @@
+def play_turn(game_board: list, x: int, y: int, piece: str):
+    if x not in [0, 1, 2] or y not in [0, 1, 2] or game_board[y][x]:
+        return False
+
+    game_board[y][x] = piece
+    return True
+
+
+if __name__ == "__main__":
+    game_board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    print(play_turn(game_board, 2, -1, "X"))
+    print(game_board)
