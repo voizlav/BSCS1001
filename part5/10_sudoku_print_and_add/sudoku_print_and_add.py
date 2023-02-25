06ea1a461766dce4d138cab915acda8f9adb869b
Vojislav Trujkic <voizlav@gmail.com>
2023-02-25 02:24:54 +0100

    Add sudoku_print_and_add

---
+++ part5/10_sudoku_print_and_add/sudoku_print_and_add.py
@@ -0,0 +1,33 @@
+def print_sudoku(sudoku: list):
+    for x in range(len(sudoku)):
+        for i in range(len(sudoku[x])):
+            print(f"{sudoku[x][i] if not sudoku[x][i] == 0 else '_'} ", end="")
+            print(" ", end="") if (i + 1) % 3 == 0 else None 
+        print() if (x + 1) % 3 == 0 else None    
+        print()
+
+
+def add_number(sudoku: list, row_no: int, column_no: int, number:int):
+    sudoku[row_no][column_no] = number
+
+
+if __name__ == "__main__":
+    sudoku  = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    print_sudoku(sudoku)
+    add_number(sudoku, 0, 0, 2)
+    add_number(sudoku, 1, 2, 7)
+    add_number(sudoku, 5, 7, 3)
+    print()
+    print("Three numbers added:")
+    print()
+    print_sudoku(sudoku)
