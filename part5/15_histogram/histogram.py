ebd108626a8dda9ecd5375874a4a65e1fa76cb40
Vojislav Trujkic <voizlav@gmail.com>
2023-02-25 08:43:33 +0100

    Update histogram

--- part5/15_histogram/histogram.py
+++ part5/15_histogram/histogram.py
@@ -1,12 +1,13 @@
 def histogram(string: str):
     result = {}
+    
     for letter in string:
         if letter not in result:
             result[letter] = ""
         result[letter] += "*"
     
     for k, i in result.items():
-        print(f"{k} {i}") 
+        print(k, i) 
 
 
 if __name__ == "__main__":
