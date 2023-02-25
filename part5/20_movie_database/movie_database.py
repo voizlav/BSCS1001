37ec83ad18f017afb5f9f12915e3728620ebff9b
Vojislav Trujkic <voizlav@gmail.com>
2023-02-25 10:53:05 +0100

    Add movie_database

---
+++ part5/20_movie_database/movie_database.py
@@ -0,0 +1,9 @@
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    database.append({ "name": name, "director": director, "year": year, "runtime": runtime})
+
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
