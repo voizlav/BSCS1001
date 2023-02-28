963521323706e22a9ae346c44cf1b70891735cd6
Vojislav Trujkic <voizlav@gmail.com>
2023-02-28 04:26:22 +0100

    Add older people

---
+++ part5/25_older_people/older_people.py
@@ -0,0 +1,17 @@
+def older_people(people: list, year: int) -> list:
+    result = []
+    for person in people:
+        if person[1] < year:
+            result.append(person[0])
+    return result
+
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    older = older_people(people, 1979)
+    print(older)
