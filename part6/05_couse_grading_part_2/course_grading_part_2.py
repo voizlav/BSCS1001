043fa249f6e4a39bc68dddcdc65c558b08e68872
Vojislav Trujkic <voizlav@gmail.com>
2023-03-02 19:15:21 +0100

    Add course_grading_part_2

---
+++ part6/05_couse_grading_part_2/course_grading_part_2.py
@@ -0,0 +1,51 @@
+data = {}
+
+if True:
+    student_info = input("Student information: ")
+    exercise_data = input("Exercises completed: ")
+    exam_points = input("Exam points: ")
+else:
+    student_info = "students1.csv"
+    exercise_data = "exercises1.csv"
+    exam_points = "exam_points1.csv"
+
+with open(student_info) as s:
+    for i in s:
+        line = i.split(";")
+        if line[0] == "id":
+            continue
+        data[int(line[0])] = [f"{line[1]} {line[2].strip()}"]
+
+with open(exercise_data) as e:
+    for i in e:
+        line = i.split(";")
+        if line[0] == "id":
+            continue
+        data[int(line[0])].append(sum(int(x.strip()) for x in line[1:]))
+
+with open(exam_points) as e:
+    for i in e:
+        line = i.split(";")
+        if line[0] == "id":
+            continue
+        data[int(line[0])].append(sum(int(x.strip()) for x in line[1:]))
+
+for id in data:
+    exercise_points = int((data[id][1] / 40) * 10) 
+    total_points = data[id][2] + exercise_points
+    grade = None
+
+    if total_points < 15:
+        grade = 0
+    elif total_points < 18:
+        grade = 1
+    elif total_points < 21:
+        grade = 2
+    elif total_points < 24:
+        grade = 3
+    elif total_points < 28:
+        grade = 4
+    else:
+        grade = 5
+    
+    print(data[id][0], grade)
